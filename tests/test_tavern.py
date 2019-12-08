36a829e8d2c4fb97903df4206c06dd93b46848cb
Dougal Scott <dougal.scott@gmail.com>
2019-12-08 21:21:42 +1100

    Start adding tests

--- tests/test_tavern.py
+++ tests/test_tavern.py
@@ -15,4 +15,15 @@ class Test_Tavern(unittest.TestCase):
         """ Test staff provisioning """
         self.assertEqual(len(self.tavern.staff), self.tavern.num_staff)
 
+
+##############################################################################
+class Test_Populate(unittest.TestCase):
+    """ Test populate """
+    def test_stools(self):
+        """ test stools creation """
+        tvn = tavern.Tavern(size_x=11, size_y=12, num_stools=1)
+        self.assertEqual(tvn.size_x, 11)
+        self.assertEqual(tvn.size_y, 12)
+        self.assertEqual(len(tvn.stools), 1)
+
 # EOF
